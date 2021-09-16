39b085a10ede3a150db9bf5e2f1b6d10a66f1e80
Emma Jin <emjin@users.noreply.github.com>
2021-09-15 21:42:42 -0700

    Add tests

---
+++ semgrep-core/interview-bug-pair-tests/normal.py
@@ -0,0 +1,9 @@
+class A:
+    def __init__(self):
+        # ruleid: return-in-init
+        return None
+
+class B:
+    def __init__(self):
+        # ok: return-in-init
+        self.inited = True
